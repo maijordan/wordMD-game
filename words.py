88228d1987816225220551991f0b8ec574e8c818
aprilchao <aprilemeraldchao@gmail.com>
2022-04-23 16:59:49 -0700

    adding word validation and hit logic

--- words.py
+++ words.py
@@ -17,7 +17,7 @@ class WordObject:
             letter = chr(randint(0, 25) + ord("a"))
             self.__letters.insert(idx, letter)
 
-    def __str__(self):
+    def info(self):
         return (
             self.__ogWord
             + "("
@@ -26,6 +26,26 @@ class WordObject:
             + " -> "
             + "".join(self.__letters)
         )
+
+    def __str__(self):
+        return "".join(self.__letters) + "(" + str(self.__numInfected) + ")"
+
+    def length(self):
+        return len(self.__letters)
+
+    def check(self):
+        "returns 1 if is a valid word, 0 if is not a valid word, or -1 if there are still infected letters"
+        if self.__numInfected > 0:
+            return -1
+        return "".join(self.__letters) in wordsSet
+
+    def hitLetter(self, i):
+        "removes letter at index i if possible, else returns False"
+        if self.__numInfected >= 1:
+            self.__letters.pop(i)
+            self.__numInfected -= 1
+            return True
+        return False
 
 
 class WordGenerator:
@@ -57,9 +77,23 @@ def main():
     """ Main function (for testing) """
 
     wg = WordGenerator()
-    words = wg.generateWords(20, 3, 10, 1, 2)
-    for w in words:
-        print(w)
+    # words = wg.generateWords(20, 3, 10, 1, 2)
+    # for w in words:
+    #     print(w.info())
+
+    while True:
+        word = wg.generateWords(1, 3, 7, 1, 3)[0]
+        print("".join(list(map(str, range(word.length())))))
+        print(word)
+
+        while word.check() == -1:
+            i = int(input())
+            word.hitLetter(i)
+            print("".join(list(map(str, range(word.length())))))
+            print(word)
+
+        print("CORRECT" if word.check() == 1 else "WRONG")
+        print()
 
 
 if __name__ == "__main__":
