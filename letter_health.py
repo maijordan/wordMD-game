3777f06f13315efc1be896a03c827b3776bb9151
aprilchao <aprilemeraldchao@gmail.com>
2022-05-04 11:39:55 -0700

    added letter sprites

--- letter_health.py
+++ letter_health.py
@@ -2,6 +2,7 @@ import arcade
 
 #change to change max health of the letters
 maxHealth = 50
+yOffset = 30
 
 class LetterHealth(arcade.Sprite):
 
@@ -13,10 +14,10 @@ class LetterHealth(arcade.Sprite):
         
     def currentHealthBar(self):
         if self.currentHealth < maxHealth:
-            arcade.draw_rectangle_filled(center_x=self.center_x, center_y=self.center_y + 25, width = maxHealth, height = 7, color = arcade.color.RED)
+            arcade.draw_rectangle_filled(center_x=self.center_x, center_y=self.center_y + yOffset, width = maxHealth, height = 7, color = arcade.color.RED)
         
         #current bar = (current/max) * maxHealth to get percentage health left
         currentBar = maxHealth * (self.currentHealth/maxHealth)
         
                                     #honestly don't know how -.5 * (maxHealth - currentBar) works
-        arcade.draw_rectangle_filled(center_x=self.center_x - 0.5 * (maxHealth - currentBar), center_y=self.center_y + 25, width = currentBar, height = 7, color = arcade.color.GREEN)
+        arcade.draw_rectangle_filled(center_x=self.center_x - 0.5 * (maxHealth - currentBar), center_y=self.center_y + yOffset, width = currentBar, height = 7, color = arcade.color.GREEN)
