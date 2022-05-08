0733e68096f8a8e8184b8b47382b768fcdaa438d
JordanMai <jordanmai927@gmail.com>
2022-05-08 16:10:05 -0700

    Updated where background music is placed

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import arcade
 from views.game_start_view import GameStartView
+import time
 
 # CONSTANTS
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "wordMD"
-
+musicList = ["resources/sounds/driftveil.mp3"]
 
 def main():
     """ Main function """
@@ -14,7 +15,19 @@ def main():
     start_view = GameStartView()
     window.show_view(start_view)
     arcade.run()
+    play_song()
 
 
 if __name__ == "__main__":
     main()
+    
+
+def play_song(self):
+    current = None
+    music = None
+    print(f"Playing {musicList[0]}")
+    if self.music:
+        self.music.stop()
+    self.music = arcade.Sound(musicList[0], streaming = True)
+    self.current = self.music.play(volume = 0.2, loop = True)
+    time.sleep(.03)
